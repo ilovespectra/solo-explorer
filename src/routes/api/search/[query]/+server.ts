930ff5ca78b7c15e7e93d38283411f309d5cb93b
tanny <94848716+ilovespectra@users.noreply.github.com>
2023-11-06 07:26:35 -0700

    Update +server.ts

--- src/routes/api/search/[query]/+server.ts
+++ src/routes/api/search/[query]/+server.ts
@@ -1,37 +1,14 @@
 import { json, type RequestEvent } from "@sveltejs/kit";
-import fetch from "node-fetch"; // Import the 'node-fetch' library
 
 import { search, connect } from "$lib/xray";
 
-// Update the proxy URL
-const PROXY_URL = "https://rpc-proxy.denverhnt.workers.dev";
+const { HELIUS_API_KEY } = process.env;
 
 // Consume a search, return what to do with it
 export async function GET({ params }: RequestEvent) {
-    const conection = connect("mainnet", ""); // Remove the HELIUS_API_KEY parameter
+    const conection = connect("mainnet", HELIUS_API_KEY);
 
-    const query = params?.query || "";
+    const result = await search(params?.query || "", conection);
 
-    try {
-        // Make an HTTP request to the proxy URL
-        const response = await fetch(`${PROXY_URL}/?query=${query}`, {
-            headers: {
-                "Content-Type": "application/json",
-            },
-            method: "GET",
-        });
-
-        if (response.ok) {
-            // Parse the response JSON
-            const result = await response.json();
-            return json(result);
-        } else {
-            throw new Error(
-                `Failed to fetch data from proxy: ${response.status}`
-            );
-        }
-    } catch (error) {
-        // console.error("Error fetching data from proxy:", error);
-        return json({ error: "Failed to fetch data from the proxy." });
-    }
+    return json(result);
 }
