262d65ff636b57036ee991764706c8ee5fd3fba8
tanny <94848716+ilovespectra@users.noreply.github.com>
2023-11-06 18:15:07 -0700

    Update +layout.svelte

--- src/routes/account/[account]/+layout.svelte
+++ src/routes/account/[account]/+layout.svelte
@@ -2,7 +2,7 @@
     import { page } from "$app/stores";
 
     import Icon from "$lib/components/icon.svelte";
-
+    import { onMount } from "svelte";
     import AccountHeader from "$lib/components/account-header.svelte";
     import { showModal } from "$lib/state/stores/modals";
     import { trpcWithQuery } from "$lib/trpc/client";
@@ -12,6 +12,93 @@
 
     const account = $page.params.account;
     const accountInfo = client.accountInfo.createQuery(account);
+
+    import { writable } from 'svelte/store';
+    import { initializeApp } from 'firebase/app';
+    import {
+        getFirestore,
+        collection,
+        doc,
+        setDoc,
+        serverTimestamp,
+        query,
+        where,
+        getDocs,
+    } from 'firebase/firestore';
+
+    const comments = writable<{ comment: string }[]>([]);
+
+
+    // Firebase configuration
+    const firebaseConfig = {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET
+    };
+    const app = initializeApp(firebaseConfig);
+
+    const signature = $page.params.account;
+
+    // Wallet and comment-related variables
+    // const { publicKey } = useWallet();
+    const comment = writable('');
+    let displayedComments: { comment: string }[] = [];
+
+    const fetchComments = async () => {
+        const db = getFirestore(app);
+        const commentsRef = collection(db, 'actcomment');
+        const commentsQuery = query(commentsRef, where('account', '==', signature));
+        
+        try {
+            const querySnapshot = await getDocs(commentsQuery);
+
+            const newComments: { comment: string }[] = querySnapshot.docs
+                .map((doc) => doc.data() as { comment: string })
+                .filter((comment) => {
+                    const isDisplayed = displayedComments.some(
+                        (displayedComment) => displayedComment.comment === comment.comment
+                    );
+                    return !isDisplayed;
+                });
+
+            // Add the new comments to the displayed comments list
+            displayedComments = displayedComments.concat(newComments);
+
+            // Update the comments store
+            comments.update((currentComments) => [...currentComments, ...newComments]);
+        } catch (error) {
+            // Handle any errors if necessary
+        }
+    };
+
+onMount(() => {
+        fetchComments(); 
+});
+
+const submitComment = async () => {
+        const commentText = $comment;
+        if (commentText) {
+            const db = getFirestore(app);
+            const commentsRef = collection(db, 'actcomment');
+            const docRef = doc(commentsRef); 
+
+            try {
+                await setDoc(docRef, {
+                    account: signature,
+                    comment: commentText,
+                    timestamp: serverTimestamp(),
+                });
+                comment.set('');
+                fetchComments(); // Fetch comments to update the list
+            } catch (error) {
+                // Handle the error if necessary
+            }
+        }
+};
 </script>
 
 <div class="relative mx-auto w-full max-w-2xl pb-32">
@@ -19,7 +106,28 @@
         {account}
         link={$page.url.href}
     />
-
+    <div
+                    class="mt-3 mb-5grid items-center gap-3 rounded-lg border p-1 py-3"
+                >
+    <h2 class="text-lg font-semibold md:text-sm ml-10 lowercase"><b>add comment</b></h2>
+        <!-- <p>Logged in as: {publicKey?.toBase58()}</p> -->
+        <textarea
+            class="text-input mt-5 ml-10"
+            placeholder="write your comment here"
+            bind:value={$comment} 
+            style="background-color: #696969"
+        ></textarea><br>
+        <button class="btn lowercase mb-10 mt-5 ml-10" on:click={submitComment}>Submit Comment</button>
+        {#if $comments.length > 0}<div><p></p></div>
+    <!-- ... -->
+    
+    {#each $comments as comment (comment.timestamp)}
+    <div class="mb-3 ml-10 px-3 badge mr-1">
+        <p style="font-size: 16px;">{comment.comment}</p>
+    </div>    
+    {/each}
+{/if}
+</div>
     <div>
         <div
             class="mx-3 mb-5 mt-3 flex items-center justify-between rounded-lg border"
@@ -28,27 +136,27 @@
                 <div />
                 <a
                     href={`/account/${account}`}
-                    class="tab-bordered tab"
+                    class="tab tab-bordered"
                     class:tab-active={$page.url.pathname.endsWith(`${account}`)}
-                    >Transactions</a
+                    >transactions</a
                 >
                 <a
                     href={`/account/${account}/tokens`}
-                    class="tab-bordered tab"
+                    class="tab tab-bordered"
                     class:tab-active={$page.url.pathname.endsWith("/tokens")}
-                    >Tokens</a
+                    >tokens</a
                 >
-                <!-- <a
+                <a
                     href={`/account/${account}/nfts`}
                     class="tab tab-bordered"
                     class:tab-active={$page.url.pathname.endsWith("/nfts")}
                     >nfts</a
-                > -->
+                >
                 <!-- <a
-                    href={`/account/${account}/comment`}
+                    href={`/account/${account}/journal`}
                     class="tab tab-bordered"
-                    class:tab-active={$page.url.pathname.endsWith("/comment")}
-                    >comment</a
+                    class:tab-active={$page.url.pathname.endsWith("/journal")}
+                    >journal</a
                 >
                 <a
                     href={`/account/${account}/entries`}
@@ -59,16 +167,16 @@
                 {#if $accountInfo?.data?.value?.owner === ACCOUNT_COMPRESSION_ID.toBase58()}
                     <a
                         href={`/account/${account}/concurrent-merkle-tree`}
-                        class="tab-bordered tab"
+                        class="tab tab-bordered"
                         class:tab-active={$page.url.pathname.endsWith(
                             "concurrent-merkle-tree"
-                        )}>concurrent merkle tree</a
+                        )}>Concurrent Merkle Tree</a
                     >
                 {/if}
             </div>
             {#if !$page.url.pathname.endsWith("/tokens")}
                 <button
-                    class="btn-ghost btn-sm btn lowercase"
+                    class="btn btn-ghost btn-sm lowercase"
                     on:click={() => showModal("TRANSACTION_FILTER")}
                 >
                     <Icon id="settings" />
