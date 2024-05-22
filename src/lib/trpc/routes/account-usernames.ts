7df26a2e7356dcd5eabd0d9917be48b46a0959ab
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-05-22 19:08:49 -0400

    Update account-usernames.ts

--- src/lib/trpc/routes/account-usernames.ts
+++ src/lib/trpc/routes/account-usernames.ts
@@ -1,27 +1,16 @@
 import { t } from "$lib/trpc/t";
+
 import { z } from "zod";
 
+import { VITE_HELIUS_API_KEY } from "$env/static/private";
+
 interface Username {
-    type: "bonfida" | "backpack";
+    type: "bonfida";
     username: string;
 }
 
-const getBackpackUsername = async (usernames: Username[], address = "") => {
-    const response = await fetch(
-        `https://xnft-api-server.xnfts.dev/v1/users/fromPubkey?publicKey=${address}&blockchain=solana`
-    );
-    const data = await response.json();
-
-    if (data?.user?.username) {
-        usernames.push({
-            type: "backpack",
-            username: data.user.username,
-        });
-    }
-};
-
 const getSolanaDomain = async (usernames: Username[], address = "") => {
-    const url = `https://rpc-proxy.denverhnt.workers.dev/v0/addresses/${address}/names`;
+    const url = `https://api.helius.xyz/v0/addresses/${address}/names?api-key=${VITE_HELIUS_API_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
 
@@ -47,11 +36,7 @@ export const accountUsernames = t.procedure
     )
     .query(async ({ input: address }) => {
         const usernames: Username[] = [];
-        await getBackpackUsername(usernames, address);
         await getSolanaDomain(usernames, address);
 
         return usernames || [];
     });
-// please replace the api key with this api proxy url
-
-// https://rpc-proxy.denverhnt.workers.dev
