3acddf104fb9f0dfdd82b826d57ead848d130998
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-05-22 18:59:27 -0400

    Update price.ts

--- src/lib/trpc/routes/price.ts
+++ src/lib/trpc/routes/price.ts
@@ -5,9 +5,9 @@ import { t } from "$lib/trpc/t";
 export const price = t.procedure
     .input(z.string())
     .query(async ({ input: token }) => {
-        const apiKey = import.meta.env.VITE_NEXT_PUBLIC_BIRDEYE_API_KEY; 
+        const apiKey = import.meta.env.VITE_BIRDEYE_API_KEY; 
         const response = await fetch(
-            `https://public-api.birdeye.so/public/price/?address=${token}`,
+            `https://public-api.birdeye.so/defi/price/?address=${token}`,
             {
                 headers: {
                     Accept: "application/json",
