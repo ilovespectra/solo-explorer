760cca8f664ac50a6030e0db70d0bb48af9141d8
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-06-27 18:56:27 -0500

    Update menu.svelte

--- src/lib/components/modals/menu.svelte
+++ src/lib/components/modals/menu.svelte
@@ -1,5 +1,6 @@
 {#each [
     { name: "home", url: "/" }, 
+{ name: "docs", url: "https://solo-docs.vercel.app/" },
 { name: "contribute", url: "https://github.com/ilovespectra/solo-explorer" }, 
 { name: "follow us on solarplex", url: "https://www.solarplex.xyz/profile/solo.live.solarplex.xyz" },
 { name: "About Helius APIs", url: "https://helius.xyz" }] as { name, url }}
