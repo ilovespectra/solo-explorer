9275d36eade8e84f33814c816a4541faa6ca16ee
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-07-11 13:17:57 -0400

    Update menu.svelte

--- src/lib/components/modals/menu.svelte
+++ src/lib/components/modals/menu.svelte
@@ -1,9 +1,10 @@
 {#each [
     { name: "home", url: "/" }, 
-{ name: "docs", url: "https://solo-docs.vercel.app/" },
-{ name: "contribute", url: "https://github.com/ilovespectra/solo-explorer" }, 
-{ name: "follow us on solarplex", url: "https://www.solarplex.xyz/profile/solo.live.solarplex.xyz" },
-{ name: "About Helius APIs", url: "https://helius.xyz" }] as { name, url }}
+    { name: "x", url: "https://x.com/soloexplorerxyz" },
+    { name: "docs", url: "https://solo-docs.vercel.app/" },
+    { name: "build", url: "https://github.com/ilovespectra/solo-explorer" }, 
+    { name: "links", url: "https://linktr.ee/soloxyz" },
+    { name: "helius", url: "https://helius.xyz" }] as { name, url }}
     <a
         href={url}
         data-sveltekit-reload
